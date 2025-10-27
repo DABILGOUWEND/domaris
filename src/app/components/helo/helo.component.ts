7033054ccf3fe9ddfdba200fbc83f7b5f4e0c99e
dabilgou10 <dabilgou10@gmail.com>
2025-10-27 21:52:33 +0000

    hello

---
+++ src/app/components/helo/helo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-helo',
+  imports: [],
+  templateUrl: './helo.component.html',
+  styleUrl: './helo.component.scss'
+})
+export class HeloComponent {
+
+}
